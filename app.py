fe4ffe0b9fb387115ddb7be1f25ad92cd1bbb916
Moibe <moi_estrello@hotmail.com>
2024-04-19 21:50:55 -0600

    Add openpyxl

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import yearDivision
 #Función principal
 def greet(name):
 
-    #yearDivision.divideYear()
+    yearDivision.divideYear()
         
     return "Tervetuloa " + name + "!!"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+iface.launch()
