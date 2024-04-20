0c6e53890c516eb8ce8eba213c9e6a4e22e2dc17
Moibe <moi_estrello@hotmail.com>
2024-04-20 02:33:44 -0600

    Year Div check

--- yearDivision.py
+++ yearDivision.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import time
 
 def divideYear():
+
+    print("Entré correctamente a yearDivision...")
 
     # Read Excel file into a DataFrame
     df = pd.read_excel('all.xlsx')
@@ -13,10 +16,25 @@ def divideYear():
 
     # Bucle anidado para iterar sobre los años
     for año in años:
+
+        print("Estoy en el año: ", año)
+        time.sleep(1)
     
         df_anio = df[df['FECHA'].dt.year == año]
+        print("Obtuve df_anio el dataset.")
+        time.sleep(10)
+        print("Es esto: ", df_anio)
         # Save the filtered data to a new Excel file
-        df_anio.to_excel(f"{df_anio}.xlsx", index=False)
+        #df_anio.to_excel(f"{df_anio}.xlsx", index=False)
 
     return "OK"
+
+
+if __name__ == "__main__":
+    # Code that should only run when the script is executed directly
+    print("Running the script directly...")
+    divideYear()
+
+
+
 
